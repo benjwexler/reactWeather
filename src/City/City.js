ccd19f189802503fc545618b9b692982894c8f97
Ben Wexler <mixedywex@gmail.com>
2018-12-17 09:29:43 -0500

    nothing

--- src/City/City.js
+++ src/City/City.js
@@ -4,27 +4,27 @@ const city = (props) => {
 
     return (
 
-        <div id={`city${props.id}`}className="CityParentContainer"
-        onMouseDown={props.lock} 
-        onTouchStart={props.lock} 
-        onMouseUp={props.move} 
-        onTouchEnd={props.move} 
-        onMouseMove={props.drag}
-        onTouchMove={props.drag} 
-        onWheel={props.preventScroll}
+        <div id={`city${props.id}`} className="CityParentContainer"
+            onMouseDown={props.lock}
+            onTouchStart={props.lock}
+            onMouseUp={props.move}
+            onTouchEnd={props.move}
+            onMouseMove={props.drag}
+            onTouchMove={props.drag}
+            onWheel={props.preventScroll}
         >
-                <div  className="City">
-                    <div className="cityContainerLeft">
-                        <div className="cityTime"> {props.time}</div>
-                        <div className="cityName">{props.city}</div>
-                        
-                    </div>
-                    <div className="cityContainerRight">
-                        <p>{props.temp}°</p>
-                    </div>
+            <div className="City">
+                <div className="cityContainerLeft">
+                    <div className="cityTime"> {props.time}</div>
+                    <div className="cityName">{props.city}</div>
+
                 </div>
-                <div onClick={props.click} onTouchStart={props.click} className="deleteBtn">
-                    Delete
+                <div className="cityContainerRight">
+                    <p>{props.temp}°</p>
+                </div>
+            </div>
+            <div onClick={props.click} onTouchStart={props.click} className="deleteBtn">
+                Delete
                 </div>
         </div>
     )
